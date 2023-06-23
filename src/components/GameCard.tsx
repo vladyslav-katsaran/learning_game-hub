c223bb4d1f852148fab9d1c7e393b4309ae6e6e0
Vladyslav Katsaran <v@katsaran.com>
2023-06-24 02:24:35 +0300

    Building games cards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,20 @@
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+
+interface Props {
+  game: Game;
+}
+
+function GameCard({ game }: Props) {
+  return (
+    <>
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={game.background_image} />
+        <CardBody>
+          <Heading size={"lg"}>{game.name}</Heading>
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+export default GameCard;
